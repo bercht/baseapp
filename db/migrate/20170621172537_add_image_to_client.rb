982dc1354b8fe2d276a42277f78b5c46aee3ee82
bercht@live.com <bercht@live.com>
2018-02-18 18:36:42 +0000

    Deploy from Forestry.io

--- db/migrate/20170621172537_add_image_to_client.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToClient < ActiveRecord::Migration
-  def change
-    add_column :clients, :image, :string
-  end
-end
--- db/migrate/20170621173623_add_omniauth_to_clients.rb
+++
@@ -1,7 +0,0 @@
-class AddOmniauthToClients < ActiveRecord::Migration
-  def change
-    add_column :clients, :name, :string
-    add_column :clients, :provider, :string
-    add_column :clients, :uid, :string
-  end
-end
